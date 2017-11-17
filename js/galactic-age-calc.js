c887a0de4c0ba107823b415866d917f0a7044236
Gyles Batara <gyles.batara@live.com>
2017-11-17 11:20:47 -0800

    Add index file and javascript interface

---
+++ js/galactic-age-calc.js
@@ -0,0 +1,23 @@
+var seconds = function(number) {
+  return number * 31556952;
+};
+
+var mercury = function(number) {
+  return number * 0.24;
+};
+
+var venus = function(number) {
+  return number * 0.62;
+};
+
+var mars = function(number) {
+  return number * 1.88;
+};
+
+var jupiter = function(number) {
+  return number * 11.86;
+};
+
+var deathDay = function(number) {
+  return 79 - number;
+};
